0272baaecc29c6538aa6077838d5c6e24d9a3ac4
Jacob Johnson <jtj60@txstate.edu>
2021-03-14 23:07:32 -0500

    final push

--- project1/src/main/java/project1/package-info.java
+++ project1/src/main/java/project1/package-info.java
@@ -1,5 +1,13 @@
 /** This package contains skeleton of project1 classes
-  * Add team members here
+  
+  William Kunesh
+  Jacob Johnson
+  Caden Westmoreland
+  
   * Add project description here
+  This project was designed for our group members to implement various methods in order to complete the code skeleton. The goal of the program is to take in
+  a dataset that contains three fields: a reviewer ID, a product ID, and the rating that the reviewer gave to the product. With this data, the methods we implemented 
+  performed various calculations to return a few different key stats: the degree (number of times that an ID appeared within our dataset), reviewer average, and product 
+  average that we then outputted. 
 */
 package project1;
